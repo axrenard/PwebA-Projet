8eb40d7f58edc45dec5cb558c6412488c435594d
axrenard <55838721+axrenard@users.noreply.github.com>
2020-02-19 19:40:06 +0100

    19/2

--- pweb/recepteur.java
+++ pweb/recepteur.java
@@ -7,7 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/*import à ajouter*/
 import org.json.*;
+import java.net.*;
+import java.io.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Servlet implementation class recepteur
@@ -33,10 +42,10 @@ public class recepteur extends HttpServlet {
 		ServletConfig config = getServletConfig();
 		HttpSession session = request.getSession();
 		
-		JSONObject reception = newJSONObject(request);
+		JSONObject reception = new JSONObject(request);
+		/*stockage reception*/
 		
-		
-		
+		response.status(HttpServletResponse.SC_OK);
 		this.getServletContext().getRequestDispatcher("").forward(request, response);
 		response.getWriter().append(" ");
 	}
@@ -45,7 +54,38 @@ public class recepteur extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		try 
+		{
+		  final DocumentBuilder builder = factory.newDocumentBuilder();
+		}
+		catch (final ParserConfigurationException e) 
+		{
+		  e.printStackTrace();
+		}
+		final Document document= builder.parse(new File(request));
+		try 
+		{
+		  final DocumentBuilder builder = factory.newDocumentBuilder();		
+		  final Document document = builder.parse(new File("repertoire.xml"));
+		}
+		catch (final ParserConfigurationException e) 
+		{
+		  e.printStackTrace();
+		}
+		catch (final SAXException e) 
+		{
+		  e.printStackTrace();
+		}
+		catch (final IOException e) 
+		{
+		  e.printStackTrace();
+		}
+		final Element reception = document.getDocumentElement();
+		/*stokage reception*/
+		
+		response.status(HttpServletResponse.SC_OK);
+		this.getServletContext().getRequestDispatcher("").forward(request, response);
+		response.getWriter().append(" ");
+		//doGet(request, response);
 	}
 }
